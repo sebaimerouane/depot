d61c030784183d49195f9f6b2eb99b4672572de6
a160450 <mohamed-lamine.sebai@worldline.com>
2016-07-29 14:16:05 +0200

    Add log4j2

--- src/main/java/com/sebai/jsfBeans/ViewEmployeesManagedBean.java
+++ src/main/java/com/sebai/jsfBeans/ViewEmployeesManagedBean.java
@@ -6,12 +6,16 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+//Import log4j classes.
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import com.sebai.data.Employee;
 
 @ManagedBean
 @SessionScoped
 public class ViewEmployeesManagedBean {
+	private static final Logger logger = LogManager.getLogger(ViewEmployeesManagedBean.class);
 	private List<Employee> employees = new ArrayList<Employee>();
 
 	public ViewEmployeesManagedBean(){
@@ -20,6 +24,7 @@ public class ViewEmployeesManagedBean {
 
 	@PostConstruct
 	public void populateEmployeeList(){
+		logger.info("Populate Employee List !");
 		for(int i = 1 ; i <= 10 ; i++){
 			Employee emp = new Employee();
 			emp.setEmployeeId(String.valueOf(i));
